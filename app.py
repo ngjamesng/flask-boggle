60baea9f45ce5afe61c9916afdf5dca3311213eb
Paige Godfrey <paagodfrey@gmail.com>
2020-01-24 14:41:17 -0800

    step two complete

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, session
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['SECRET_KEY'] = "oh-so-secret"
 
 # debug = DebugToolbarExtension(app)
 
@@ -11,4 +11,7 @@ app = Flask(__name__)
 def display_board():
     boggle_game = Boggle()
     board = boggle_game.make_board()
+
+    session['board'] = board
+
     return render_template('index.html', board=board)
