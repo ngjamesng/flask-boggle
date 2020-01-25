c10637a8cbf7dcb13eea5d6bdaa18293685a7fce
ngjamesng <just.james.ng@gmail.com>
2020-01-24 16:33:36 -0800

    temporarily remove flash message, add event listener for word submission, add route changed post route to fully respond

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, flash, redirect, session
+from flask import Flask, request, render_template, flash, redirect, session, jsonify
+
 # from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config["SECRET_KEY"] = "oh-so-secret"
 
 # debug = DebugToolbarExtension(app)
+boggle_game = Boggle()
 
-@app.route('/')
+
+@app.route("/")
 def display_board():
-    boggle_game = Boggle()
     board = boggle_game.make_board()
 
-    session['board'] = board
+    session["board"] = board
+    session["successful_words"] = []
+    return render_template("index.html", board=board)
 
-    return render_template('index.html', board=board)
 
-@app.route('/', methods=["POST"])
+@app.route("/", methods=["POST"])
 def submit_word():
-    word_input = request.json['word-input']
-    return word_input
+    word_input = request.json["word-input"]
 
+    # if word already submitted, return "not okay"
+
+    # if word is valid,
+    board = session["board"]
+    result_value = boggle_game.check_valid_word(board, word_input)
+
+    if result_value == "ok":
+        msg = f"added: {word_input}"
+    elif result_value == "not-on-board":
+        msg = f"{word_input} is not a valid word on this board"
+    elif result_value == "not-word":
+        msg = f"{word_input} is not a valid English word"
+    # return jsonify("result": msg)
+    return jsonify(result=result_value)
+    #  not a valid english word
+    # added: "word"
+    # not a valid word on this board
+    # flash(f"{word_input} is {result}")
 
--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,24 @@
-const $highScore = $('#high-score');
-const $currentScore = $('#current-score');
-const $playCount = $('#play-count');
-const $timer = $('#timer')
-const $wordForm = $('#word-form')
-const $wordInput = $("#word-input")
+const $highScore = $("#high-score");
+const $currentScore = $("#current-score");
+const $playCount = $("#play-count");
+const $timer = $("#timer");
+const $wordForm = $("#word-form");
+const $wordInput = $("#word-input");
+const $messageContainer = $("#message-container");
 
 // get user input
 
-$wordForm.on('submit', async function (evt) {
-  evt.preventDefault();
-  let word = $wordInput.val();
-  response = await axios.post('/', {'word-input': word});
+$wordForm.on("submit", async function(evt) {
+	evt.preventDefault();
+	let word = $wordInput.val();
+	response = await axios.post("/", { "word-input": word });
+	// word_validity = await axios.get()
+	console.log("message: ", response.data);
+  showMessage(response.data.result);
+
 });
 
+function showMessage(message) {
+	let messageDiv = $(`<div>${message}</div>`);
+	$messageContainer.empty().append(messageDiv);
+}
